b36141255790867b6cb8753a68ba88ef31be93f9
rohitghatal1 <rohitghatal@gmail.com>
2024-07-04 16:37:12 +0545

    added javaScript for automatic typing effect for pruducts

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const products = document.getElementById("products");
+const allProducts = ["Laptop", "Mobile", "Smart Watches"];
+let arrayIndex = 0;
+let charIndex = 0;
+let typingDelay = 200;
+let erasingDelay = 100;
+let newTextDelay = 2000;
+
+function type(){
+    if (charIndex < allProducts[arrayIndex].length){
+        products.textContent += allProducts[arrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase(){
+    if(charIndex > 0){
+        products.textContent = allProducts[arrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else{
+        arrayIndex = (arrayIndex + 1) % allProducts.length;
+        setTimeout(type, typingDelay)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, newTextDelay);
+});
